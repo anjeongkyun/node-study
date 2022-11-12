0ccb47ec6648955334a2ccb1d00f9cebed7a3917
dkswjdrbs12@naver.com <dkswjdrbs12@naver.com>
2022-11-12 16:17:39 +0900

    feat: users get api 추가

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var app = express();
+var morgan = require('morgan');
+var users = [
+    {id:1, name: 'alice'},
+    {id:2, name: 'bek'},
+    {id:3, name: 'chris'}
+]
+
+app.use(morgan('dev'));
 
 app.listen(3000, function() {
     console.log('listening on port 3000 !');
 });
 
-app.get('/', function(req, res) {
-    res.send('hello world');
+app.get('/users', function(req, res) {
+    console.log('/users');
+    res.json(users);
 });
