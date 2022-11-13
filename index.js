194b09730cb59aca043ed6b17a6422c20ba5cf71
dkswjdrbs12@naver.com <dkswjdrbs12@naver.com>
2022-11-13 23:21:55 +0900

    test: get /users test code 작성

--- index.js
+++ index.js
@@ -14,7 +14,14 @@ app.listen(3000, function() {
 });
 
 app.get('/users', function(req, res) {
-    res.json(users);
+    req.query.limit = req.query.limit || 10;
+    const limit = parseInt(req.query.limit, 10);
+
+    if(Number.isNaN(limit)){
+        return res.status(400).end();
+    }
+
+    res.json(users.slice(0, limit));
 });
 
 module.exports = app;
